0150ed9bbb744fc78f3caf8047b39d32d7f56a40
bomatson <bobby@carbonfive.com>
2014-05-24 17:22:36 -0700

    start off with some views and a basic node app

---
+++ web.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+  req.accepts('application/json');
+  res.render('drums', { title: 'Leap Drums!' });
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
